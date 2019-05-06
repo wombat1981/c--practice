e9adb2d1f6f1aaf0b5e23d5334d760dbcc222e00
wombat1981 <48079366+wombat1981@users.noreply.github.com>
2019-05-06 18:23:32 +1000

    43

---
+++ python/python/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace python
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            string number = Console.ReadLine();
+            int numb = int.Parse(number);
+
+            if (numb % 2 == 0) 
+            {
+                Console.WriteLine("This is an even number");
+            }
+
+            else
+	            {
+                    Console.WriteLine("This number is odd");
+
+                }
+
+            
+
+            Console.ReadLine();
+
+            
+
+
+            /*
+           Exercise 2 (and Solution)
+Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+
+If the number is a multiple of 4, print out a different message.
+Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+             */
+        }
+    }
+}
---
+++ variable task 2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Enter your first name");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter your Surname");
+            string Lastname = Console.ReadLine();
+            Console.WriteLine("Enter your user id");
+            string userid = Console.ReadLine();
+            Console.WriteLine("Enter your Year of birth");
+            string birthyear = Console.ReadLine();
+            Console.WriteLine("Enter the current year");
+            string currentyear = Console.ReadLine();
+
+            int id = int.Parse(userid);
+            int Birth = int.Parse(birthyear);
+            int Current = int.Parse(currentyear);
+            int total = (Current - Birth);
+            
+            Console.WriteLine("Hello " + firstname +"\n" + Lastname + "\n" + id + "\n" +  total );
+            Console.ReadLine();
+
+
+
+
+            /*
+                        Write a C# Console application that has the following functionality:
+
+            Ask for and read the user's first name;
+            Ask for and read the user's surname;
+            Ask for and read the user's studentID;
+            Ask for and read the user's year of birth;
+            Ask for and read the current year;
+                        Display the user's first name and surname and age;
+            Example output:
+
+            Enter first name:
+
+                        Joe
+
+                        Enter surname:
+
+                        Bloggs
+
+                        Enter StudentID:
+
+                        12345678
+
+            Enter year of birth:
+
+            1950
+
+            Enter current year:
+
+                        2017
+
+
+
+
+            Joe Bloggs
+
+            67
+            */
+        }
+    }
+}
